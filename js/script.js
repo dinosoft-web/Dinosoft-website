25e459a006190ca85620f79967e5130b6f25268f
Gradon <123156149+dinosoft-web@users.noreply.github.com>
2023-04-06 14:07:13 +0200

    adding services

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@ const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
 const mdMenu = document.getElementById('md-menu')
 const mdBtn = document.getElementById('md-btn')
+const tabs = document.querySelectorAll('.tab')
+const panels = document.querySelectorAll('.panel')
 btn.addEventListener('click', navToggle)
 mdBtn.addEventListener('click', menuToggle)
+
+// Tabs menu event listener
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
 
 function navToggle() {
 	btn.classList.toggle('open')
@@ -15,4 +20,26 @@ function menuToggle() {
 	console.log('pouet')
 	mdBtn.classList.add('md:hidden')
 	mdMenu.classList.add('md:flex')
+}
+
+function onTabClick(e) {
+	// Deactivate all tabs
+	tabs.forEach((tab) => {
+		tab.children[0].classList.remove(
+			'border-orange',
+			'border-b-4',
+			'md:border-b-0'
+		)
+	})
+
+	// Hide all panels
+	panels.forEach((panel) => panel.classList.add('hidden'))
+
+	// Activate a new tab and panel based on the target
+	e.target.classList.add('border-orange', 'border-b-4')
+	const classString = e.target.getAttribute('data-target')
+	document
+		.getElementById('panels')
+		.getElementsByClassName(classString)[0]
+		.classList.remove('hidden')
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
 	content: ['./*.html'],
 	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '1020px',
+			xl: '1440px',
+		},
 		extend: {
 			fontFamily: {
 				sans: ['Open Sans', 'sans-serif'],
